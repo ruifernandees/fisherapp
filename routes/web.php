36d6f0246b1cbd8a846005d2b9b52cced032454b
ruifernandees <dev.ruifernandes@gmail.com>
2020-10-31 15:39:49 -0300

    Add Auth namespace to web.php (routes)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,14 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('list-user', [UserController::class, 'listUser']);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
