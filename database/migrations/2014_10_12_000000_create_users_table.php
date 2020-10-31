8532d292562e430397a13dfb265e15648fbd62cb
ruifernandees <dev.ruifernandes@gmail.com>
2020-10-31 11:27:49 -0300

    Update create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf', 11)->unique();
+            $table->string('phone', 11)->unique();
+            $table->string('profile_photo');
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
