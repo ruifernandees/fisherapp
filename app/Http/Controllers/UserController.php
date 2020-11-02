562890ffabc812597229d86567714ffebdd57c12
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-02 18:05:58 -0300

    Implement index and update methods: UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-    public function listUser()
+    public function index()
     {
-        $user = new User();
-        echo "<h1>User List</h1>";
+        return view('user.index', [
+            'user' => Auth::user()
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->except('_token', '_method');
+
+        $currentUserId = Auth::user()->id;
+
+        $user = User::find($currentUserId);
+
+        if ($data['password']) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        if (isset($data['profile_photo'])) {
+            Storage::delete("usersPhotos/{$user->profile_photo}");
+
+            $request->file('profile_photo')->store('usersPhotos');
+            $data['profile_photo'] = $request->file('profile_photo')->hashName();
+        }
+
+        $toUpdate = array_filter($data, function($item) {
+            return $item;
+        });
+
+        $wasUpdated = $user->update($toUpdate);
+
+        if ($wasUpdated) {
+            return redirect(route('user.index'))->with('status', 'Perfil atualizado com sucesso!');
+        }
+
+        return redirect(route('user.index'))->with('error', 'Erro inesperado');
     }
 }
