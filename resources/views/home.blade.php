a8b856c3199d01eb4da4690e75fac86c4e01a832
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-02 17:08:40 -0300

    Remove rankings from home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,74 +79,7 @@
                     </table>
                     <a href="{{ route('fishes.create') }}">Adicionar peixe</a>
 
-                    <h2 style="margin-top: 20px;">Classificações</h2>
-                    <h3>Por quantidade de peixes</h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Posição</th>
-                                <th>Nome</th>
-                                <th>Pescou</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>🥇</th>
-                                <th>1º lugar</th>
-                                <th><a href="#">Rui</a></th>
-                                <th>20 peixes</th>
-                            </tr>
-                            <tr>
-                                <th>🥈</th>
-                                <th>2º lugar</th>
-                                <th><a href="#">José</a></th>
-                                <th>15 peixes</th>
-                            </tr>
-                            <tr>
-                                <th>🥉</th>
-                                <th>3º lugar</th>
-                                <th><a href="#">João</a></th>
-                                <th>10 peixes</th>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <h3>Pelo mais pesado peixe pescado</h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Posição</th>
-                                <th>Nome</th>
-                                <th>Peixe</th>
-                                <th>Peso</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>🥇</th>
-                                <th>1º lugar</th>
-                                <th><a href="#">Rui</a></th>
-                                <th><a href="#">Dourado-do-mar</a></th>
-                                <th>22kg</th>
-                            </tr>
-                            <tr>
-                                <th>🥈</th>
-                                <th>2º lugar</th>
-                                <th><a href="#">João</a></th>
-                                <th><a href="#">Truta-arco-íris</a></th>
-                                <th>12kg</th>
-                            </tr>
-                            <tr>
-                                <th>🥉</th>
-                                <th>3º lugar</th>
-                                <th><a href="#">José</a></th>
-                                <th><a href="#">Carpa-comum</a></th>
-                                <th>10kg</th>
-                            </tr>
-                        </tbody>
-                    </table>
+                    
                 </div>
             </div>
         </div>
