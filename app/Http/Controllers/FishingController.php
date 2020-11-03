4a0aa204c5e233186e8c5547c23d5a4b1b558574
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-03 12:19:54 -0300

    Implement FishingController::update method

--- app/Http/Controllers/FishingController.php
+++ app/Http/Controllers/FishingController.php
@@ -96,4 +96,59 @@ class FishingController extends Controller
         
         return back()->withInput()->withErrors(['Erro inesperado']);
     }
+
+    public function update(Request $request)
+    {
+        $data = $request->except(['_token', '_method', 'id', 'friends']);
+
+        $fishingId = $request->only('id')['id'];
+        $fishing = Fishing::find($fishingId);
+
+        $fishingUsers = $fishing->users;
+        
+        if (isset($request->only('friends')['friends'])) {
+            $friendsFromRequest = $request->only('friends')['friends'];
+
+            $friendsFromDB = [];
+            foreach ($fishingUsers as $key => $user) {
+                if ($user->id != Auth::user()->id) {
+                    $friendsFromDB[$key] = strval($user->id);
+                }
+            }
+
+            $missingInDB = array_diff($friendsFromRequest, $friendsFromDB);
+
+            if ($missingInDB) {
+                foreach ($missingInDB as $friendId) {
+                    UserFishing::create([
+                        'user_id' => $friendId,
+                        'fishing_id' => $fishing->id
+                    ]);
+                }
+            }
+            
+            $missingInRequest = array_diff($friendsFromDB, $friendsFromRequest);
+
+            if ($missingInRequest) {
+                foreach ($missingInRequest as $friendId) {
+                    DB::table('users_fishings')
+                        ->where('user_id', '=', $friendId)
+                        ->where('fishing_id', '=', $fishing->id)
+                        ->delete();
+                }
+            }
+        }
+
+        $toUpdate = array_filter($data, function($item) {
+            return $item;
+        });
+
+        $wasUpdated = $fishing->update($toUpdate);
+
+        if ($wasUpdated) {
+            return redirect(route('fishings.show', ['id' => $fishingId]))->with('status', 'Pescaria atualizada com sucesso!');
+        }
+
+        return redirect(route('fishings.show', ['id' => $fishingId]))->with('error', 'Erro inesperado');
+    }
 }
