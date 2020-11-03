acdd8c428389e4dcbd00fb99cd77604731b4bfea
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-03 12:18:52 -0300

    Implement FishingController::show

--- app/Http/Controllers/FishingController.php
+++ app/Http/Controllers/FishingController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\UserFishing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class FishingController extends Controller
@@ -26,10 +27,41 @@ class FishingController extends Controller
         ]);
     }
 
+    public function show(string $id)
+    {
+        $currentUserId = Auth::user()->id;
+
+        $currentUser = User::find($currentUserId);
+        
+        $fishing = Fishing::findOrFail($id);
+
+        $fishingLocation = [$fishing->latitude, $fishing->longitude];
+
+        $allUsers = User::all();
+
+        foreach ($allUsers as $key => $user) {
+            if ($user->id == Auth::user()->id) {
+                unset($allUsers[$key]);
+            }
+        }
+
+        $selectedFriends = $fishing->users;
+
+        $selectedFriendsId = [];
+        foreach ($selectedFriends as $selectedFriend) {
+            $selectedFriendsId[] = $selectedFriend->id;
+        }
+
+        return view('fishings.show', [
+            'fishing' => $fishing,
+            'fishingLocation' => $fishingLocation,
+            'allUsers' => $allUsers,
+            'selectedFriendsId' => $selectedFriendsId
+        ]);
+    }
+
     public function store(Request $request)
     {
-        // var_dump($request->except(['_token']));
-
         $fishingData = $request->except(['_token', 'friends']);
 
         if (isset($request->only(['friends'])['friends'])) {
