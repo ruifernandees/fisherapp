fab463ce1b0fdcb65549b133aefb478c0c2b9844
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-03 12:46:08 -0300

    Add fisherapp logo to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'FisherApp') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img 
+                        style="width: 150px;"
+                        src="{{ asset('images/fisherapp-logo.png') }}" 
+                        alt="FisherApp logo"
+                    />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
